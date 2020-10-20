8b53618e7751083cfd26d3fb89fad3e5bd83fd7a
Alejandro Dopp <doppalej@msu.edu>
2020-10-20 19:56:07 -0400

    Likes, Retweets, and Tweets

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class HomeTableViewController: UITableViewController {
 
     
+    @IBOutlet var TweetTable: UITableView!
     
     var tweetArray = [NSDictionary]()
     var numberOfTweet: Int!
@@ -20,10 +21,12 @@ class HomeTableViewController: UITableViewController {
         super.viewDidLoad()
         loadTweet()
 
-        
     }
     
-    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweet()
+    }
     
     func loadTweet(){
         
@@ -70,6 +73,9 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
         
